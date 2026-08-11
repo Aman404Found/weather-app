92b8c68f7fbac012114809e400066c9b95fd93fe
404AmanFound <sant.sharmawelding@gmail.com>
2026-08-11 12:10:33 +0530

    netlify.toml added

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-/* Generated during build time */
+/* Generated automatically during build time */
 export const CONFIG = {
-    WEATHER_API_KEY: '',
+    WEATHER_API_KEY: 'ACEVYVWVQ3WN3MURAGMTVQRBN',
     BASE_URL: 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
 };
 
